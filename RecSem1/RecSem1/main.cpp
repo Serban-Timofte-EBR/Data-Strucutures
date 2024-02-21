d256e7c5db0c7aa903dae6cb23f32d661e440de5
Serban Timofte <serbantimofte@Serbans-MacBook-Air.local>
2024-02-21 15:03:33 +0200

    Ref Seminar1 Complet

--- RecSem1/RecSem1/main.cpp
+++ RecSem1/RecSem1/main.cpp
@@ -1,7 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 #include <malloc.h>
-#include <string>
+#include <string.h>
 
 
 typedef struct {
@@ -52,7 +53,52 @@ void dezalocare_vector(produs* vp, int nr) {
 	free(vp);
 }
 
+void afisare4Vectori(int* coduri, char** denumiri, float* preturi, float* cantitati, int nr)
+{
+	for (int i = 0; i < nr; i++)
+		printf("\nCod = %d, Denumire = %s, Pret = %5.2f, Cantitate = %5.2f",
+			coduri[i], denumiri[i], preturi[i], cantitati[i]);
+}
+
+void citireMatrice(float** mat, char** denumiri, int nr)
+{
+	char buffer[20];
+	for (int i = 0; i < nr; i++)
+	{
+		printf("Cod=");
+		scanf("%f", &mat[i][0]);
+		printf("Denumire=");
+		scanf("%s", buffer);
+		denumiri[i] = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(denumiri[i], buffer);
+		printf("Pret=");
+		scanf("%f", &mat[i][1]);
+		printf("Cantitate=");
+		scanf("%f", &mat[i][2]);
+	}
+}
+
+void afisareMatrice(float** mat, char** denumiri, int nr)
+{
+	for (int i = 0; i < nr; i++)
+		printf("\nCod = %5.2f, Denumire = %s, Pret = %5.2f, Cantitate = %5.2f",
+			mat[i][0], denumiri[i], mat[i][1], mat[i][2]);
+}
+
+void dezalocareMatrice(float** mat, char** denumiri, int nr)
+{
+	for (int i = 0; i < nr; i++)
+	{
+		free(mat[i]);
+		free(denumiri[i]);
+	}
+	free(mat);
+	free(denumiri);
+}
+
+
 int main() {
+	/*
 	produs* vp = nullptr;
 	int nr;
 	printf("Numarul de elemente din vector: ");
@@ -66,6 +112,59 @@ int main() {
 	printf("\nValoarea totala a vectorului: %f", valoare_totala(vp, nr));
 
 	dezalocare_vector(vp, nr);
+	*/
 
+	FILE* f; 
+	f = fopen("date.txt", "r");
+	
+	if (f == NULL) {
+		printf("No such file or directory!");
+		return -1;
+	}
+
+	produs* vpDoc = nullptr;
+	int nrDoc;
+	fscanf(f, "%d", &nrDoc);
+
+	vpDoc = (produs*)malloc(nrDoc * sizeof(produs));
+
+	char buffer[20];
+	for (int i = 0; i < nrDoc; i++) {
+		fscanf(f, "%d", &vpDoc[i].cod);
+		fscanf(f, "%s", buffer);
+		vpDoc[i].den = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(vpDoc[i].den, buffer);
+		fscanf(f, "%f", vpDoc[i].pret);
+		fscanf(f, "%f", vpDoc[i].cant);
+	}
+
+	int nr;
+	printf("Numarul de elemente din vector: ");
+	scanf("%d", &nr);
+
+	int* coduri = (int*)malloc(nr * sizeof(int));
+	char** denumiri = (char**)malloc(nr * sizeof(char*));
+	float* preturi = (float*)malloc(nr * sizeof(float));
+	float* cantitate = (float*)malloc(nr * sizeof(float));
+
+	char buffer2[20];
+	for (int i = 0; i < nr; i++) {
+		fscanf(f, "%d", &coduri[i]);
+		fscanf(f, "%s", buffer2);
+		denumiri[i] = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(denumiri[i], buffer2);
+		fscanf(f, "%f", &preturi[i]);
+		fscanf(f, "%f", cantitate[i]);
+	}
+
+	float** mat = (float**)malloc(nr * sizeof(float*));
+	for (int i = 0; i < nr; i++)
+		mat[i] = (float*)malloc(3 * sizeof(float));
+
+	citireMatrice(mat, denumiri, nr);
+	afisareMatrice(mat, denumiri, nr);
+	dezalocareMatrice(mat, denumiri, nr);
+
+	fclose(f); 
 	return 0;
 }
