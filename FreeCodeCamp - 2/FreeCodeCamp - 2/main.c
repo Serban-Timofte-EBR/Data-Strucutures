f478f50eeae15083287a1093f8b391d33eb8cd80
Serban Timofte <serbantimofte@Serbans-MacBook-Air.local>
2024-03-06 18:33:06 +0200

    Recuperarea seminar 3 prof. Ciurea. Lista dubla inlatuita + Stergere element dintr-o lista dublu inlatuita + Lista circulara

---
+++ FreeCodeCamp - 2/FreeCodeCamp - 2/main.c
@@ -0,0 +1,168 @@
+﻿//Linked List: Inserting a node at beginning
+//Linked List: Inserting a node at nth position
+//Linked List: Delete a node at nth position
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct {
+	int data;
+	struct Node* next;
+} Node;
+
+struct Node* head;
+struct Node* head2;
+
+void Insert_Begin(int x) {
+	Node* temp = (Node*)malloc(sizeof(Node));
+
+	temp->data = x;	
+	//temp->next = NULL;
+	//To cover both scenarios: 
+	temp->next = head;
+
+	head = temp;
+
+	//SCENARIO: List is empty
+	//head = temp;
+
+	//SCENARIO: List already has a first element
+	//if (head != NULL) {
+	//	temp->next = head;
+	//}
+
+	//head = temp;
+}
+
+void Print() {
+	//Use reference to not use the head next element
+	Node* temp = head;
+
+	while (temp != NULL) {
+		printf(" %d", temp->data);
+		temp = temp->next;
+	}
+
+	printf("\n");
+}
+
+void Insert(int data, int n) {
+	Node* temp1 = (Node*)malloc(sizeof(Node));
+
+	temp1->data = data;
+	temp1->next = NULL;
+
+	if (n == 1) {
+		temp1->next = head;	//if the list is not empty
+		head = temp1;
+		return;
+	}
+
+	Node* temp2 = head;
+	for (int i = 0; i < n - 2; i++) {
+		temp2 = temp2->next;
+	}
+
+	temp1->next = temp2->next;	//Elementul nou creat pointeaza catre viitorul element din lista
+	temp2->next = temp1;		//Actualul element n-1 pointeaza catre noul element de la pozitia n
+}
+
+void Print2() {
+	Node* temp = head;
+
+	while (temp != NULL) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+
+	printf("\n");
+}
+
+void Insert_End(int data) {
+	Node* temp1 = (Node*)malloc(sizeof(Node));
+
+	temp1->data = data;
+	temp1->next = NULL;
+
+	if (head2 == NULL) {
+		temp1->next = head2;
+		head2 = temp1;
+		return;
+	}
+
+	Node* temp2 = head2;
+	while (temp2->next != NULL) {
+		temp2 = temp2->next;
+	}
+
+	temp2->next = temp1;
+}
+
+void Print3() {
+	Node* temp = head2;
+
+	if (head2 == NULL) { printf("List is empty"); }
+
+	while (temp != NULL) {
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+void Delete(int index) {
+	Node* temp1 = head2;
+
+	if (index == 1) {
+		head2 = temp1->next;
+		free(temp1);
+		return;
+	}
+
+	for (int i = 0; i < index - 2; i++) {
+		temp1 = temp1->next;
+	}
+
+	Node* temp2 = temp1->next;
+	temp1->next = temp2->next;	//n+1 Node
+	free(temp2);
+}
+
+int main() {
+	head = NULL;	//empty list
+
+	//Linked List: Inserting a node at beginning
+
+	/*printf("How many numbers there are in the list?\n");
+	int n, x;
+	scanf("%d", &n);*/
+	
+	/*for (int i = 0; i < n; i++) {
+		printf("Enter the number\t\n");
+		scanf("%d", &x);
+		Insert_Begin(x);
+		Print();
+	}*/
+
+	//Linked List: Inserting a node at nth position
+	Insert(2, 1);	//List: 2
+	Insert(3, 2);	//List: 2, 3
+	Insert(4, 1);	//List: 4, 2, 3
+	Insert(5, 2);	//List: 4, 5, 2, 3
+	Print2();
+
+	head2 = NULL;
+
+	Insert_End(2);
+	Insert_End(4);
+	Insert_End(6);
+	Insert_End(5);	//List: 2,4,6,5
+	Print3();
+
+	int n;
+	printf("\nEnter a position: \n");
+	scanf("%d", &n);
+	Delete(n);
+	Print3();
+
+	return 0;
+}
---
+++ RecSem3/RecSem3/main.c
@@ -0,0 +1,203 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+typedef struct {
+	int* anFabricatie;
+	char* marca;
+	float consumMediu;
+} masina;
+
+typedef struct {
+	masina inf;
+	struct nodeLS* next;
+	struct nodeLS* prev;
+} nodeLS;
+
+nodeLS* inserareNod(nodeLS* head, nodeLS** tail, masina m) {
+	//create a new node
+	nodeLS* nou = (nodeLS*)malloc(sizeof(nodeLS));
+
+	//copy the information in the node inf
+	nou->inf.anFabricatie = (int*)malloc(sizeof(int));
+	*(nou->inf.anFabricatie) = *(m.anFabricatie);
+
+	nou->inf.marca = (char*)malloc((strlen(m.marca) + 1) * sizeof(char));
+	strcpy(nou->inf.marca, m.marca);
+
+	nou->inf.consumMediu = m.consumMediu;
+
+	if (head == NULL) {
+		nou->next = nou;
+		nou->prev = nou;
+		head = nou;
+		*tail = nou;
+	}
+	else {
+		nodeLS* temp = head;
+		while (temp->next != head) {
+			temp = temp->next; 
+		}
+
+		temp->next = nou;
+		nou->prev = temp;
+		*tail = nou;
+		(*tail)->next = head;
+		head->prev = *tail;
+	}
+
+	printf("Node inserted!\n");
+	return head;
+}
+
+void traversareLista(nodeLS* head) {
+	printf("List in Normal Order: \n\n");
+	nodeLS* temp = head;
+	while (temp->next != head) {
+		printf("An fabricatie = %d, \t Marca = %s, \t Consum Mediu = %d\n",
+			*(temp->inf.anFabricatie), temp->inf.marca, temp->inf.consumMediu);
+		temp = temp->next;
+	}
+	printf("An fabricatie = %d, \t Marca = %s, \t Consum Mediu = %d\n",
+		*(temp->inf.anFabricatie), temp->inf.marca, temp->inf.consumMediu);
+}
+
+void traversareListaInvers(nodeLS* tail) {
+	printf("List in Reverse Order: \n\n");
+	nodeLS* temp = tail;
+	while (temp->prev != tail) {
+		printf("An fabricatie = %d, \t Marca = %s, \t Consum Mediu = %d\n",
+			*(temp->inf.anFabricatie), temp->inf.marca, temp->inf.consumMediu);
+		temp = temp->prev;
+	}
+	printf("An fabricatie = %d, \t Marca = %s, \t Consum Mediu = %d\n",
+		*(temp->inf.anFabricatie), temp->inf.marca, temp->inf.consumMediu);
+}
+
+void saveListInVector(nodeLS* head, masina* vec, int* nrElem, int anFabricatie) {
+	nodeLS* temp = head;
+	*nrElem = 0;
+	while (temp->next != head) {
+		if (*(temp->inf.anFabricatie) >= anFabricatie) {
+			vec[*nrElem].anFabricatie = temp->inf.anFabricatie;
+			vec[*nrElem].marca = (char*)malloc((strlen(temp->inf.marca) + 1)
+				* sizeof(char));
+			strcpy(vec[*nrElem].marca, temp->inf.marca);
+			vec[*nrElem].consumMediu = temp->inf.consumMediu;
+			(*nrElem)++;
+		}
+		temp = temp->next;
+	}
+	if (*(temp->inf.anFabricatie) >= anFabricatie) {
+		vec[*nrElem].anFabricatie = temp->inf.anFabricatie;
+		vec[*nrElem].marca = (char*)malloc((strlen(temp->inf.marca) + 1)
+			* sizeof(char));
+		strcpy(vec[*nrElem].marca, temp->inf.marca);
+		vec[*nrElem].consumMediu = temp->inf.consumMediu;
+	}
+}
+
+void deleteElem(nodeLS** head, nodeLS** tail, char* marca) {
+	//delete first node
+	if (strcmp((*head)->inf.marca, marca) == 0) {
+		nodeLS* temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL) {
+			(*head)->prev = NULL;
+		}
+		free(temp->inf.anFabricatie);
+		free(temp->inf.marca);
+		free(temp);
+		return;
+	}
+	else {
+		//delete last node
+		if (strcmp((*tail)->inf.marca, marca) == 0) {
+			nodeLS* temp = *tail;
+			*tail = (*tail)->prev;
+			if (*tail != NULL) {
+				(*tail)->next = NULL;
+			}
+			free(temp->inf.anFabricatie);
+			free(temp->inf.marca);
+			free(temp);
+		}
+		//delete middle element
+		else {
+			nodeLS* temp = *head;
+			while (temp != NULL) {
+				if (strcmp(temp->inf.marca, marca) == 0) {
+					nodeLS* anterior = temp->prev;
+					nodeLS* urmator = temp->next;
+					anterior->next = urmator;
+					urmator->prev = anterior;
+					free(temp->inf.anFabricatie);
+					free(temp->inf.marca);
+					free(temp);
+					return;
+				}
+				else {
+					temp = temp->next;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	int nrMasini;
+	nodeLS* head = NULL;
+	nodeLS* tail = NULL;
+	masina m;
+	char buffer[20];
+	FILE* fisier = fopen("input.txt", "r");
+
+	if (fisier == NULL) {
+		printf("File is not opened!");
+		return -1;
+	}
+
+	fscanf(fisier, "%d", &nrMasini);
+	for (int i = 0; i < nrMasini; i++) {
+		m.anFabricatie = (int*)malloc(sizeof(int));
+		fscanf(fisier, "%d", m.anFabricatie);
+		
+		fscanf(fisier, "%s", buffer);
+		m.marca = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy(m.marca, buffer);
+
+		fscanf(fisier, "%f", &m.consumMediu);
+
+		head = inserareNod(head, &tail, m);
+		free(m.anFabricatie);
+		free(m.marca);
+	}
+	fclose(fisier);
+
+	printf("\n------------------\n");
+
+	traversareLista(head);
+
+	printf("\n------------------\n");
+
+	traversareListaInvers(tail);
+
+	printf("\n------------------\n");
+
+	deleteElem(&head, &tail, "Hyundai");
+	traversareLista(head);
+
+	printf("\n------------------\n");
+
+	int nrElem;
+	masina* vecMasini = (masina*)malloc(nrMasini * sizeof(masina));
+	saveListInVector(head, vecMasini, &nrElem, 2020);
+
+	printf("Cars in vector: \n\n");
+	for (int i = 0; i < nrElem; i++)
+		printf("\nAn fabricatie = %d, Marca = %s, Consum mediu = %5.2f",
+			*(vecMasini[i].anFabricatie), vecMasini[i].marca, vecMasini[i].consumMediu);
+	printf("\n");
+	return 0;
+}
